5d0135450db5d534eba091e35cbb12f39a951c0f
swadhinrajbehera@gmail.com <swadhinrajbehera@gmail.com>
2026-02-23 04:22:58 +0530

    arithmatic.php

---
+++ arithmatic.php
@@ -0,0 +1,45 @@
+//1. Addition (+)
+
+Adds two numbers.
+
+<?php
+$a = 10;
+$b = 5;
+echo $a + $b;  // Output: 15
+?>
+
+
+//2. Subtraction (-)
+<?php
+$a = 10;
+$b = 5;
+echo $a - $b;  // Output: 5
+?>
+
+//3. Multiplication (*)
+<?php
+$a = 10;
+$b = 5;
+echo $a * $b;  // Output: 50
+?>
+
+//4. Division (/)
+<?php
+$a = 10;
+$b = 5;
+echo $a / $b;  // Output: 2
+?>
+
+//5. Modulus (%)
+<?php
+$a = 10;
+$b = 3;
+echo $a % $b;  // Output: 1
+?>
+
+//6. Exponentiation (**)
+<?php
+$a = 2;
+$b = 3;
+echo $a ** $b;  // Output: 8
+?>
