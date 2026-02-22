cb050aca014e182c0072cb5f9ba1d75348e8cd72
swadhinrajbehera@gmail.com <swadhinrajbehera@gmail.com>
2026-02-23 04:22:19 +0530

    assigop

---
+++ assigop.php
@@ -0,0 +1,18 @@
+<?php
+$x = 20;
+
+$x += 5;
+echo "After += : $x <br>";
+
+$x -= 3;
+echo "After -= : $x <br>";
+
+$x *= 2;
+echo "After *= : $x <br>";
+
+$x /= 4;
+echo "After /= : $x <br>";
+
+$x %= 3;
+echo "After %= : $x <br>";
+?>
