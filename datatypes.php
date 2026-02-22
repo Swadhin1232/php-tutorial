07bfb9d52725e0b5ee361cae9ca566b7f2e72f8c
swadhinrajbehera@gmail.com <swadhinrajbehera@gmail.com>
2026-02-23 03:04:56 +0530

    datatypes

---
+++ datatypes.php
@@ -0,0 +1,17 @@
+<?php
+
+
+$a="hello world";//string
+
+/*echo $a."<br>";
+$a=98; //int
+echo $a."<br>";
+$a=2.3;// flot
+echo $a."<br>";
+*/
+
+
+var_dump($a);// to know the data type of variable
+
+
+?>
