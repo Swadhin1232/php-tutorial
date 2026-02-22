2d9602b3ce87fbb42a3ca738d2ce2ec048300de1
swadhinrajbehera@gmail.com <swadhinrajbehera@gmail.com>
2026-02-23 04:21:21 +0530

    echoprint dif

---
+++ echoprint.php
@@ -0,0 +1,14 @@
+<?php
+echo"hello world" . " <br>";
+echo"hello" ,"world";// concatination allow in echo
+echo"hello "."world";
+
+
+?>
+
+
+
+/*conatination not allow
+printecho"hello "."world";
+echo"hello" ,"world"; //not allow
+*/
