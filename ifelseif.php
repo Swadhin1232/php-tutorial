d672b0d881da11198029ebc26f82b5b90c0f1013
swadhinrajbehera@gmail.com <swadhinrajbehera@gmail.com>
2026-02-25 01:49:20 +0530

    ifelseif

---
+++ ifelseif.php
@@ -0,0 +1,27 @@
+if (condition1) {
+    // code if condition1 is true
+}
+else if (condition2) {
+    // code if condition2 is true
+}
+else {
+    // code if all conditions are false
+}
+
+
+<?php
+$marks = 85;
+
+if ($marks >= 90) {
+    echo "Grade A";
+}
+else if ($marks >= 75) {
+    echo "Grade B";
+}
+else if ($marks >= 50) {
+    echo "Grade C";
+}
+else {
+    echo "Fail";
+}
+?>
