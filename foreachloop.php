eec34c56ade91c0c8b9562275ac2579d48231b24
swadhinrajbehera@gmail.com <swadhinrajbehera@gmail.com>
2026-02-26 03:15:38 +0530

    foreach

---
+++ foreachloop.php
@@ -0,0 +1,17 @@
+*/foreach ($array as $value) {
+    // code
+}
+or
+foreach ($array as $key => $value) {
+    // code
+}
+/*
+
+
+<?php
+$fruits = ["Apple", "Banana", "Mango"];
+
+foreach ($fruits as $fruit) {
+    echo $fruit . "<br>";
+}
+?>
