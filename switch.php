37fbd572e2d2dde4b49c8878225ceeaf7009ffc1
swadhinrajbehera@gmail.com <swadhinrajbehera@gmail.com>
2026-02-26 03:13:02 +0530

    switch

---
+++ switch.php
@@ -0,0 +1,41 @@
+switch (variable) {
+    case value1:
+        // code
+        break;
+
+    case value2:
+        // code
+        break;
+
+    default:
+        // code
+}
+
+<?php
+$day = 3;
+
+switch ($day) {
+    case 1:
+        echo "Monday";
+        break;
+
+    case 2:
+        echo "Tuesday";
+        break;
+
+    case 3:
+        echo "Wednesday";
+        break;
+
+    case 4:
+        echo "Thursday";
+        break;
+
+    case 5:
+        echo "Friday";
+        break;
+
+    default:
+        echo "Weekend";
+}
+?>
