35906066135a1be2e6df195d8652e8f5f4413e6b
swadhinrajbehera@gmail.com <swadhinrajbehera@gmail.com>
2026-03-06 22:50:46 +0530

    nestedloop

---
+++ nestedloop.php
@@ -0,0 +1,24 @@
+outer_loop {
+    inner_loop {
+        // code to be executed
+    }
+}
+
+// structure
+for (...) {
+    for (...) {
+        // statements
+    }
+}
+
+
+
+<?php
+
+for ($i = 1; $i <= 3; $i++) {
+    for ($j = 1; $j <= 2; $j++) {
+        echo "i = $i , j = $j <br>";
+    }
+}
+
+?>
