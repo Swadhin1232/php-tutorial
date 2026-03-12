79871b0fcd3d70dc70791365e415ac88d4aa7631
swadhinrajbehera@gmail.com <swadhinrajbehera@gmail.com>
2026-03-12 23:53:55 +0530

    inittial commit

---
+++ variablefn.php
@@ -0,0 +1,15 @@
+$variableName = "functionName";
+$variableName();   // calls the function
+
+
+2. Example of Variable Function
+<?php
+
+function sayHello() {
+    echo "Hello, Welcome to PHP!";
+}
+
+$func = "sayHello";   // variable stores function name
+$func();              // calling function using variable
+
+?>
