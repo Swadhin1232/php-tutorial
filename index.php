aeec3b6d94c73d1c2e4f8918a2e59a1c0c03af43
swadhinrajbehera@gmail.com <swadhinrajbehera@gmail.com>
2026-03-13 23:22:38 +0530

    inittial commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+include "header.php";
+
+echo "<p>This is the homepage.</p>";
+?>
+
+
+Welcome to My Website
+This is the homepage.
+
+
+
+<?php
+require "config.php";
+
+echo $site_name;
+?>
