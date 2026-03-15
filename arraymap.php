8447f4b405e891cdf4dce86ca4c93fd77b183c10
swadhinrajbehera@gmail.com <swadhinrajbehera@gmail.com>
2026-03-15 21:37:55 +0530

    inittial commit

---
+++ arraymap.php
@@ -0,0 +1,22 @@
+ <?php
+function square($n) {
+    return $n * $n;
+}
+
+$numbers = array(1, 2, 3, 4);
+
+$result = array_map("square", $numbers);
+
+print_r($result);
+?>
+
+
+<?php
+$numbers = array(1, 2, 3, 4);
+
+$result = array_map(function($n){
+    return $n * 2;
+}, $numbers);
+
+print_r($result);
+?>
